3e4673252db14f8e652e259d0fe630e7e5d6aa87
unknown <danilonaves2209@gmail.com>
2023-11-29 11:52:47 -0300

    start filter

--- Emaflix/Frontend/src/routes/pages/Filter.jsx
+++ Emaflix/Frontend/src/routes/pages/Filter.jsx
@@ -1,12 +1,17 @@
+import Cards from "../../components/pages/Card.jsx"
 
-
-import "../css/Home.css"
+import "../css/Filter.css"
 
 const Filter = () => {
     return (
-    <div className="box">
-        <h1>Pesquisa</h1>
-    </div>)
+        <div className="filter">
+            <input className="pesquisa" type="search" name="" id="" placeholder="Pesquisas por gêneros, nomes, classificação, " />
+            <h2>Títulos encontrados</h2>
+            <div className="list-cards">
+                <Cards />
+            </div>
+        </div>
+    );
 };
 
 
