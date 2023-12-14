22bc8a8b4905f2a917635aeb73be1da117563eaa
Danilo <danilonaves2209@gmail.com>
2023-12-13 23:10:37 -0300

    US [5,6]

--- Emaflix/Frontend/src/routes/pages/UsuarioConf.jsx
+++ Emaflix/Frontend/src/routes/pages/UsuarioConf.jsx
@@ -1,51 +1,76 @@
 import { useEffect, useState } from "react";
-
-import "../css/UsuarioConf.css"
+import "../css/UsuarioConf.css";
 import axios from "axios";
 
 const UsuarioConf = () => {
-    const [User, setUser] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [newAction, setNewAction] = useState('');
+    const [Conf, setConf] = useState('');
+    const [username, setUsername] = useState('');
 
     useEffect(() => {
         const getUserConf = async () => {
             try {
                 const response = await axios.get("http://localhost:3000/userconfig");
-                const UserConfig = response.data;
-                setUser(UserConfig);
+                const userConfig = response.data;
+                setUsers(userConfig);
                 console.log(response.data);
             } catch (error) {
-                console.log("Erro ao busca o conteúdo");
+                console.log("Erro ao buscar o conteúdo");
             }
         };
-        getUserConf()
-    }, [])
+        getUserConf();
+    }, []);
 
-    const handleDeletar = (userId) => {
+    const DeletarUsuario = (userId) => {
+    };
 
+    const MudarPermissao = async (userId) => {
+        if (newAction !== 'conf' && newAction !== 'user') {
+            setConf("As opçãos são: conf ou user");
+            return;
+        }
+        try {
+            const response = await axios.put(`http://localhost:3000/chargepermissao`, {
+                username: username,
+                newactions: newAction,
+            });
+            window.location.href = '/usuario_conf';
+        } catch (error) {
+            console.error('Erro ao atualizar o usuário', error);
+        }
+    };
+
+    const InputChangeUsername = (event) => {
+        event.target.value;
+        setUsername(event.target.value);
+    };
+
+    const InputChangeAction = (event) => {
+        const value = event.target.value;
+        setNewAction(value);
     };
 
     return (
         <div className="usuarios-conf">
             <h2>Permissões de usuários</h2>
+            <h3 className="aviso">{Conf}</h3>
+            <div className="permission-charge">
+                <input placeholder="Usuário:" type="text" value={username} onChange={InputChangeUsername} />
+                <input placeholder="Permissão:" type="text" value={newAction} onChange={InputChangeAction} />
+                <button onClick={MudarPermissao}>Alterar Permissão</button>
+            </div>
             <ul className="list-user">
-                {User.map(user => (
-                    <li key={user.id}>
+                {users.map(user => (
+                    <li key={user.username}>
                         <span>
                             <h3>Usuário:&nbsp;</h3>
-                            <h3 className="h3-simple">{user.user}</h3>
+                            <h3 className="h3-simple">{user.username}</h3>
                         </span>
                         <span>
                             <h3>Ação:&nbsp;</h3>
                             <h3 className="h3-simple">{user.actions === 'conf' ? "Gerenciar Vídeos" : "Aluno"}</h3>
                         </span>
-                        <select
-                            value={user.actions}
-                            onChange={(e) => handleActionChange(e, user.id)}
-                        >
-                            <option value="view">Aluno</option>
-                            <option value="conf">Gerenciar Vídeos</option>
-                        </select>
-                       <button onClick={() => handleDeletar(user.id)}> <span className="material-symbols-outlined">delete</span>Excluir</button>
                     </li>
                 ))}
             </ul>
@@ -53,6 +78,4 @@ const UsuarioConf = () => {
     )
 };
 
-
-
-export default UsuarioConf
+export default UsuarioConf;
